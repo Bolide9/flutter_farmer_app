e18e10dc982b5b1026a542618478d8235ba56400
Bolide9 <viskhaev.m@yandex.ru>
2023-05-26 20:25:46 +0300

    some fixes

--- lib/farmer/presentation/farmer_page.dart
+++ lib/farmer/presentation/farmer_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_farmer_app/ui/review_card.dart';
+import 'package:flutter_farmer_app/colors.dart';
+import 'package:flutter_farmer_app/reviews/presentation/widgets/review_card.dart';
+import 'package:line_icons/line_icons.dart';
 
 import '../../ui/expandable_page_view.dart';
 
@@ -15,22 +17,56 @@ class FarmerPage extends StatefulWidget {
 class _FarmerPageState extends State<FarmerPage> {
   late final PageController _pageController;
 
+  List<Widget> _buildReviewCards() {
+    final parts = <Widget>[];
+
+    for (int index = 0; index < 5; index++) {
+      parts.add(
+        const Padding(
+          padding: EdgeInsets.only(right: 8),
+          child: ReviewCard(
+            rating: 5,
+            userName: 'Test User',
+            reviewComment: 'Test Comment\nTest comment\n\nTest Comment',
+          ),
+        ),
+      );
+    }
+
+    return parts;
+  }
+
   @override
   void initState() {
-    _pageController = PageController(viewportFraction: 0.92);
+    _pageController = PageController(viewportFraction: 0.95);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     const green = Color(0xFF42AB44);
-    const TextStyle secondaryTextStyle = TextStyle(fontSize: 12, color: Colors.black54);
-    const TextStyle primaryBold = TextStyle(fontWeight: FontWeight.bold);
+
+    const TextStyle secondaryTextStyle = TextStyle(
+      fontSize: 12,
+      color: Colors.black54,
+    );
+
+    const TextStyle primaryBold = TextStyle(
+      fontWeight: FontWeight.bold,
+    );
 
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.share))],
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.share),
+          ),
+        ],
       ),
       body: SafeArea(
         child: SingleChildScrollView(
@@ -46,33 +82,62 @@ class _FarmerPageState extends State<FarmerPage> {
                     const Placeholder(),
                     Transform.translate(
                       offset: const Offset(0, 20),
-                      child: const CircleAvatar(radius: 40, child: Text("fe")),
+                      child: const CircleAvatar(
+                        radius: 40,
+                        child: Text("fe"),
+                      ),
                     )
                   ],
                 ),
               ),
               const SizedBox(height: 30),
               Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.symmetric(horizontal: 8),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text("Название", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 20),
                     const Text(
-                        "Love this plugin! Does exactly what it is supposed to do and so far without any real issues. (You might want to review some Dummy Text generation which contains words and even sentences with a meaning and that should not suppose to happen)"),
-                    const Text("Все описание", style: TextStyle(color: green)),
-                    const SizedBox(height: 10),
-                    const Divider(),
-                    const SizedBox(height: 10),
-
-                    // Блок условий доставки и оплаты
+                      "Название",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "Love this plugin! Does exactly what it is supposed to do and so far without any real issues. (You might want to review some Dummy Text generation which contains words and even sentences with a meaning and that should not suppose to happen)",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black.withOpacity(0.7),
+                      ),
+                      maxLines: 6,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(top: 24),
+                      child: Text(
+                        "Все описание",
+                        style: TextStyle(
+                          color: green,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Row(
                         children: const [
-                          Icon(Icons.menu),
-                          SizedBox(width: 10),
+                          Icon(
+                            LineIcons.stream,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
                           Text(
                             "Условия доставки и оплаты",
                             style: primaryBold,
@@ -90,19 +155,29 @@ class _FarmerPageState extends State<FarmerPage> {
                           children: const [
                             SizedBox(height: 10),
                             Text(
-                              "Минимальная сумма заказа - 10000.00\$",
+                              "Минимальная сумма заказа - 1000 ₽",
                               style: secondaryTextStyle,
                             ),
                             Text("Доставка по всей России", style: secondaryTextStyle),
                             SizedBox(height: 15),
-                            Text(
-                              "Оплата: Наличными, Банковской картой",
-                              style: secondaryTextStyle,
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: "Доставка по всей России",
+                                    style: secondaryTextStyle,
+                                  ),
+                                  TextSpan(
+                                    text: "Доставка по всей России",
+                                    style: secondaryTextStyle,
+                                  ),
+                                ],
+                              ),
                             ),
                             Text(
-                              "Минимальная сумма заказа - 10000.00\$",
+                              "Оплата: Наличными\nБанковской картой",
                               style: secondaryTextStyle,
-                            )
+                            ),
                           ],
                         ),
                       ),
@@ -110,14 +185,13 @@ class _FarmerPageState extends State<FarmerPage> {
                     const SizedBox(height: 10),
                     const Divider(),
                     const SizedBox(height: 10),
-                    //
-
-                    //Блок адреса доставки
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Row(
                         children: const [
-                          Icon(Icons.map),
+                          Icon(
+                            LineIcons.map,
+                          ),
                           SizedBox(width: 10),
                           Text(
                             "Адрес доставки",
@@ -128,17 +202,15 @@ class _FarmerPageState extends State<FarmerPage> {
                     ),
                     const SizedBox(height: 10),
                     const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 8.0),
-                          child: Text("г.Урус-Мартан, Бруклинский район, дом 5", style: secondaryTextStyle),
-                        )),
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 8.0),
+                        child: Text("г.Урус-Мартан, Бруклинский район, дом 5", style: secondaryTextStyle),
+                      ),
+                    ),
                     const SizedBox(height: 10),
                     const Divider(),
                     const SizedBox(height: 10),
-                    //
-
-                    //Блок отзывов и оценок
                     Align(
                       alignment: Alignment.centerLeft,
                       child: Column(
@@ -147,28 +219,43 @@ class _FarmerPageState extends State<FarmerPage> {
                           const Text("Оценки и отзывы", style: primaryBold),
                           Row(
                             children: const [
-                              Icon(Icons.star_border_rounded, color: Colors.amber),
-                              Text(" 4,9", style: TextStyle(fontSize: 24, color: green)),
-                              Text(" / 7", style: TextStyle(fontSize: 24))
+                              Icon(
+                                Icons.star_border_rounded,
+                                color: Colors.amber,
+                              ),
+                              Text(
+                                " 4,9",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: green,
+                                ),
+                              ),
+                              Text(
+                                " / 7",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                ),
+                              )
                             ],
                           ),
-                          Container(
-                            padding: const EdgeInsets.all(16.0),
-                            decoration: BoxDecoration(border: const Border(), borderRadius: BorderRadius.circular(5)),
-                            height: 150,
-                            child: ExpandablePageView(
-                              controller: _pageController,
-                              children: const [ReviewCard(), ReviewCard()],
-                            ),
+                          ExpandablePageView(
+                            controller: _pageController,
+                            children: _buildReviewCards(),
                           ),
                           const SizedBox(height: 10),
                           Center(
                             child: Padding(
                               padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                              child: ElevatedButton(onPressed: () {}, child: const Text("Связаться с продавцом", style: primaryBold)),
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Связаться с продавцом",
+                                  style: primaryBold,
+                                ),
+                              ),
                             ),
                           ),
-                          const SizedBox(height: 10)
+                          const SizedBox(height: 10),
                         ],
                       ),
                     ),
